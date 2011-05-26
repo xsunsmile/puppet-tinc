8a0204e043b3759da01a26be96ff1707afc50e7b
Hao SUN <xsunsmile@gmail.com>
2011-05-26 14:33:06 +0900

    update all modules

--- manifests/inters.pp
+++ manifests/inters.pp
@@ -1,7 +1,7 @@
 
 class tinc::inters {
 
-  inters::executable::mongofile_put { "/etc/tinc/${name}/hosts/${fqdn_tinc}":
+  inters::functions::mongofile_put { "/etc/tinc/${name}/hosts/${fqdn_tinc}":
     require => File["/etc/tinc/${name}/hosts/${fqdn_tinc}"],
   }
 
